6bbebd609207ff2919ad778c44e321b7510e46e8
Arjun Kannan <arj.shiv@gmail.com>
2016-01-25 09:30:47 -0500

    Adding solution 2

---
+++ src/2.js
@@ -0,0 +1,38 @@
+function bruteForceFib(limit) {
+    var f1 = 0;
+    var f2 = 1;
+    var f3 = 0;
+    var sum = 0;
+
+    while (sum < limit) {
+        f3 = f2 + f1;
+        f1 = f2;
+        f2 = f3;
+
+        if (f3 % 2 === 0) {
+            sum += f3;
+        }
+    }
+
+    return sum;
+}
+
+function betterBruteForceFib(limit) {
+    var f6 = 0;
+    var f3 = 2;
+    var fn = 2;
+    var sum = 0;
+
+    //every third number in the series is even
+    while (sum < limit) {
+        sum += fn;
+        fn = 4*f3 + f6;
+        f6 = f3;
+        f3 = fn;
+    }
+
+    return sum;
+}
+
+console.log(bruteForceFib(4000000));
+console.log(betterBruteForceFib(4000000));
